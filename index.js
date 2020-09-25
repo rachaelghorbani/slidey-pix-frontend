fd8ac85bf295e8c275929dd86c8def6fa4a8409d
alerner1 <alyssplerner@gmail.com>
2020-09-25 11:18:54 -0400

    canvas crop math

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 document.addEventListener('DOMContentLoaded', e => {
   console.log('loaded!')
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+
+  let imgObj = new Image();
+  imgObj.src = 'https://live.staticflickr.com/4443/37614774531_72a256658b_3k.jpg';
+
+  imgObj.onload = function() {
+    let w = canvas.width;
+    let nw = imgObj.naturalWidth;
+    let nh = imgObj.naturalHeight;
+    let h = canvas.height;
+
+    if (nh < nw) {
+      // let's say nw is 2400
+      // nh is 1600
+      // actual w is 1200
+      // actual h is 800
+      // resize ratio is w / nw = .5 = 50%
+      // so if we have w / nw
+      // and we have nw - nh = 800
+      // since the resize ratio is 50%, we need to crop 50% of nw - nh
+      // which is (w / nw) (nw - nh)
+      // and we crop half of that from each side
+      // so (w / nw) (nw - nh) / 2
+
+      const ratio = w / nw
+      const difference = nw - nh;
+      const toCrop = (ratio * difference) / 2;
+
+      ctx.drawImage(imgObj, -toCrop, 0, w - toCrop, h)
+
+    } else {
+      const difference = h - w;
+      const toCrop = difference / 2;
+
+    }
+
+    // ctx.drawImage(imgObj, 0, 0, h, h)
+  }
+
+
+ 
 })
